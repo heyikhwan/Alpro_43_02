ccbac181932b6630e77aeb099e653df2abfaf17d
heyIkhwan <ikhwanmail231000@gmail.com>
2020-05-17 22:57:00 +0700

    Update Responsi 11

--- src/ch07/a_AlgoritmaBlockSort.java
+++ src/ch07/a_AlgoritmaBlockSort.java
@@ -8,6 +8,33 @@ public class a_AlgoritmaBlockSort {
     public static void main(String[] args) {
         int[] koleksiAngka = {82, 12, 41, 38, 19, 26, 9, 48, 20, 55, 8, 32, 3};
 
-	//Algoritma Block Sort
+        blocksort(koleksiAngka);
+
+        System.out.print("Meng-urutkan Array | Algoritma Block Sort\n[ ");
+        cetakArray(koleksiAngka);
+        System.out.println("]");
+
+    }
+
+    static void blocksort(int[] koleksiAngka){
+        int length = koleksiAngka.length;
+
+        for (int i = 1; i < length; i++){
+            int n = koleksiAngka[i];
+            int j = i - 1;
+
+            while ((j > -1) && (koleksiAngka[j] > n)) {
+                koleksiAngka[j+1] = koleksiAngka[j];
+                j--;
+            }
+
+            koleksiAngka[j+1] = n;
+        }
+    }
+
+    static void cetakArray(int[] arrAngka) {
+        for (int i = 0; i < arrAngka.length; i++) {
+            System.out.print(arrAngka[i] + " ");
+        }
     }
 }
